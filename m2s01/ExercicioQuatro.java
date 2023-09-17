e7e4f556dbbde964e5737b2c4369adf623b2c037
Maria Vicente <mnvcaetano@gmail.com>
2023-09-17 20:37:32 -0300

    Resolução do exercício 4

---
+++ m2s01/ExercicioQuatro.java
@@ -0,0 +1,19 @@
+package m2s01;
+
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adivinhe em que número de 1 a 5 estou pensando!");
+        int number = scanner.nextInt();
+        int randomNumber = (int)(Math.random() * (5-1) + 1);
+
+        if (number == randomNumber) {
+            System.out.println("Parabéns! Você acertou!");
+        } else {
+            System.out.println("Ops! Número errado :(");
+        }
+    }
+}
