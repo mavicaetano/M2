1c5bf5ddc30801abe6f34b3e1edf567f9d5de0e5
Maria Vicente <mnvcaetano@gmail.com>
2023-09-17 19:40:53 -0300

    fix: insere a resolução completa

--- m2s01/ExercicioDois.java
+++ m2s01/ExercicioDois.java
@@ -1,2 +1,17 @@
-package m2s01;public class ExercicioDois {
+package m2s01;
+
+import java.util.Scanner;
+
+public class ExercicioDois {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual a sua altura?");
+        double height = scanner.nextDouble();
+        System.out.println("Qual o seu peso?");
+        double weight = scanner.nextDouble();
+        double imc = weight / (height * height);
+
+        System.out.printf("Sua altura é %.2f, seu peso é %.2f e seu IMC é %.2f.", height, weight, imc);
+    }
 }
