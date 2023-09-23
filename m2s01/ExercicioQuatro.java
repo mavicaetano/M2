dce64d7053074d81c5708639b1999dde94049e1f
Maria Vicente <mnvcaetano@gmail.com>
2023-09-23 16:55:38 -0300

    refactor: altera a estrutura do if

--- m2s01/ExercicioQuatro.java
+++ m2s01/ExercicioQuatro.java
@@ -8,12 +8,12 @@ public class ExercicioQuatro {
 
         System.out.println("Adivinhe em que número de 1 a 5 estou pensando!");
         int number = scanner.nextInt();
-        int randomNumber = (int)(Math.random() * (5-1) + 1);
+        int randomNumber = (int) (Math.random() * (5 - 1) + 1);
 
-        if (number == randomNumber) {
-            System.out.println("Parabéns! Você acertou!");
-        } else {
+        if (number != randomNumber) {
             System.out.printf("Ops! Número errado :( O número correto era %d.", randomNumber);
+            return;
         }
+        System.out.println("Parabéns! Você acertou!");
     }
 }
--- m2s01/ExercicioTres.java
+++ m2s01/ExercicioTres.java
@@ -14,6 +14,5 @@ public class ExercicioTres {
             return;
         }
             System.out.println("É ímpar!");
-        }
     }
 }
