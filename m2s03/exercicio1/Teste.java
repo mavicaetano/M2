4822862e8c4264417570f103fa6c99a90aa56801
Maria Vicente <mnvcaetano@gmail.com>
2023-12-14 22:34:39 -0300

    fix: corrige argumentos do sout

--- m2s03/exercicio1/Teste.java
+++ m2s03/exercicio1/Teste.java
@@ -4,11 +4,11 @@ public class Teste {
 
     public static void main(String[] args) {
 
-        Funcionario funcionario1 = new Funcionario(12345678900L, "Maria Vicente", 2500F);
+        m2s03.exercicio1.Funcionario funcionario1 = new m2s03.exercicio1.Funcionario(12345678900L, "Maria Vicente", 2500F);
 
-        Funcionario funcionario2 = new Funcionario(98765432100L, "Maria Caetano", 1600F);
+        m2s03.exercicio1.Funcionario funcionario2 = new m2s03.exercicio1.Funcionario(98765432100L, "Maria Caetano", 1600F);
 
         funcionario1.promover(15F);
-        System.out.printf("O salário de %s aumentou em %f. O novo valor é de %.2f", funcionario1.getNomeCompleto(), String.format(funcionario1.promover()), );
+        System.out.printf("O salário de %s aumentou. O novo valor é de %.2f", funcionario1.getNomeCompleto(), funcionario1.getSalario());
     }
 }
