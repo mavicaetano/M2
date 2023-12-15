3358a2d8ee3674be6669bae7dd823d2a68309e88
Maria Vicente <mnvcaetano@gmail.com>
2023-12-14 22:37:44 -0300

    fix: corrige o nome do package

--- m2s03/exercicio2/Teste.java
+++ m2s03/exercicio2/Teste.java
@@ -1,9 +1,9 @@
-package m2s03.exercicio2;
+package exercicio2;
 
 public class Teste {
     public static void main(String[] args) {
 
-        m2s03.exercicio2.Funcionario funcionario = new m2s03.exercicio2.Funcionario("Maria");
+        exercicio2.Funcionario funcionario = new exercicio2.Funcionario("Maria");
         funcionario.setSalario(2600F);
         funcionario.aumentar(1320F);
         System.out.println(funcionario.getSalario());
