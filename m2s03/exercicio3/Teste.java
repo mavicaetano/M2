61e86b01f04bf3e9874a47214d5c8aae96805c38
Maria Vicente <133928149+mavicaetano@users.noreply.github.com>
2023-12-16 17:51:47 -0300

    Update Teste.java

--- m2s03/exercicio3/Teste.java
+++ m2s03/exercicio3/Teste.java
@@ -3,7 +3,7 @@ package exercicio3;
 public class Teste {
     public static void main(String[] args) {
 
-        String frase = "Welcome to the dark side";
+        String frase = "Come to the dark side";
         int qtdPalavras = Contador.contarPalavras(frase);
         System.out.println(qtdPalavras);
     }
